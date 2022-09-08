f6c1025c21dd076964c80680b994421f0af23f20
nassim-sadi <nacimbreeze@gmail.com>
2022-09-08 17:15:01 +0100

    testing

--- config/db.js
+++ config/db.js
@@ -3,6 +3,8 @@ const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {});
     console.log(`Mongo Connected ${conn.connection.host}`);
+    console.log('connected to db');
+    console.error('testing console error');
   } catch (err) {
     console.log(err);
     process.exit(1);
